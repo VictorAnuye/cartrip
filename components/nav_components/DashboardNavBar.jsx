6719c8b5cd86ef8b3623c4a065a7751d66799dda
victoranuye <victoranuye@gmail.com>
2024-09-27 13:48:51 +0100

    my code is working

--- components/nav_components/DashboardNavBar.jsx
+++ components/nav_components/DashboardNavBar.jsx
@@ -2,31 +2,30 @@ import Link from "next/link";
 import { Montserrat } from "next/font/google";
 import { signOut } from "next-auth/react";
 
-
 const montserrat_thin_100 = Montserrat({
-    subsets: ["latin"],
+    subsets:["latin"],
     weight: "100"
-})
+});
 
 export function DashboardNavBar ({cssClass}) {
     return (
         <ul className={cssClass}>
-        <li>
-            <Link
-            className={`${montserrat_thin_100} text-yellow-50 text-lg`}
-            href="/dashboard">Home</Link>
-        </li>
-        <li>
-            <Link
-            className={`${montserrat_thin_100} text-yellow-50 text-lg`}
-            href="/dashbard/profile">Profile</Link>
-        </li>
-        <li>
-            <Link
-            onClick={signOut}
-            className={`${montserrat_thin_100} text-yellow-50 text-lg`}
-            href="#">Sign Out</Link>
-        </li>
-    </ul>
+            <li>
+                <Link 
+                className={`${montserrat_thin_100.className} text-yellow-50 text-lg`}
+                href="/dashboard">Home</Link>
+            </li>
+            <li>
+                <Link 
+                className={`${montserrat_thin_100.className} text-yellow-50 text-lg`}
+                href="/dashboard/profile">Profile</Link>
+            </li>
+            <li>
+                <Link 
+                onClick={signOut}
+                className={`${montserrat_thin_100.className} text-yellow-50 text-lg`}
+                href="#">Sign out</Link>
+            </li>
+        </ul>
     )
 }
--- components/nav_components/GeneralNavBar.js
+++ components/nav_components/GeneralNavBar.js
@@ -1,34 +1,36 @@
 import Link from "next/link";
-import { Montserrat } from "next/font/google";
+import {Montserrat} from "next/font/google";
 
 const montserrat_thin_100 = Montserrat({
-    subsets: ["latin"],
+    subsets:["latin"],
     weight: "100"
-})
+});
 
-export function GeneralNavBar ({cssClass}) {
+
+
+export function GeneralNavBar ({cssClass}){
     return (
         <ul className={cssClass}>
-        <li>
-            <Link
-            className={`${montserrat_thin_100} text-yellow-50 text-lg`}
-            href="/">Home</Link>
-        </li>
-        <li>
-            <Link
-            className={`${montserrat_thin_100} text-yellow-50 text-lg`}
-            href="#">About Us</Link>
-        </li>
-        <li>
-            <Link
-            className={`${montserrat_thin_100} text-yellow-50 text-lg`}
-            href="#">Contact Us</Link>
-        </li>
-        <li>
-            <Link
-            className={`${montserrat_thin_100} text-yellow-50 text-lg`}
-            href="/dashboard">Account</Link>
-        </li>
-    </ul>
+            <li>
+                <Link
+                className={`${montserrat_thin_100.className} text-yellow-50 text-lg`}
+                href="/">Home</Link>
+            </li>
+            <li>
+                <Link
+                className={`${montserrat_thin_100.className} text-yellow-50 text-lg`}
+                href="#">About us</Link>
+            </li>
+            <li>
+                <Link
+                className={`${montserrat_thin_100.className} text-yellow-50 text-lg`}
+                href="#">Contact us</Link>
+            </li>
+            <li>
+                <Link
+                className={`${montserrat_thin_100.className} text-yellow-50 text-lg`}
+                href="/dashboard">Account</Link>
+            </li>
+        </ul>
     )
 }
