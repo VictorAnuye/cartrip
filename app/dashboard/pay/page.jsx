c2bcd9814706550f15e302f643f0cd313bf842fb
victoranuye <victoranuye@gmail.com>
2024-09-29 22:12:33 +0100

    my code is working

--- app/dashboard/pay/page.jsx
+++ app/dashboard/pay/page.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link"
-export default function History () {
+
+export default function Pay () {
     return (
         <section className="py-12 px-2 md:px-6 lg:px-12">
-            <p>Hisory</p>
+        <p>Pay</p>
         </section>
     )
 }
